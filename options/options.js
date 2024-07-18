3b6d20026fe2bc0ee5bfe8c4892571b235912495
Crowdbotics <engineering@crowdbotics.com>
2024-07-18 11:04:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-surf-501",
+  url: "https://solitary-surf-501.botics.co",
+  api: "https://solitary-surf-501.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
